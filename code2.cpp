0465b03f3a1fc30372e4b95bbeeb4c1a746c13fa
Ojas45 <ojaskharke07@gmail.com>
2024-07-03 00:25:49 +0530

    Add files via upload

---
+++ code2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"To find greatest of two numbers"<<endl;
+    
+    int a;
+    int b;
+    
+    
+    cout<<"Enter first number:";
+    cin>>a;
+
+    cout<<"Enter second number:";
+    cin>>b;
+
+   
+    if(a>=b)
+      cout<<"greatest number is:"<<a<<endl;
+
+   
+    else
+      cout<<"greatest number is:"<<b<<endl;
+    return 0;
+
+}
